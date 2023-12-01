a0bdfe3f15ddcbb28978d75b04a6182eb2a78d15
444chak <444chak@gmail.com>
2023-12-01 09:02:10 +0100

    ⏫ pylint + upgrade DAY1

--- adventofcode23/day1/day1.py
+++ adventofcode23/day1/day1.py
@@ -1,11 +1,21 @@
+"""
+Advent of code 2023 - Day 1 - Part 1 & 2
+https://adventofcode.com/2023/day/1
+"""
+
 import os
 
-# get path of the file 
+# get path of the file
 path = os.path.dirname(os.path.realpath(__file__))
 FILE = path + "/data.txt"
+
+
 def part1():
-    with open(FILE) as f:
-        sum = 0
+    """
+    Part 1
+    """
+    with open(FILE, encoding='utf-8') as f:
+        total = 0
         for i in f.readlines():
             nb = []
             for j in i:
@@ -14,16 +24,15 @@ def part1():
                     nb.append(j)
 
             nb = nb[0] + nb[-1]
-            sum += int(nb)
+            total += int(nb)
 
-    print(sum)
+    print(total)
 
-    
 part1()
 
 
 # bonus
-print(sum(int(nb[0] + nb[-1]) for nb in [list(filter(str.isdigit, i)) for i in open(FILE)]))
+print(sum(int(nb[0] + nb[-1]) for nb in [list(filter(str.isdigit, i)) for i in open(FILE, encoding='utf-8')]))
 
 
 numbers = {
@@ -39,25 +48,27 @@ numbers = {
 }
 
 def part2():
-    with open(FILE) as f:
-        sum = 0
+    """
+    Part 2
+    """
+    with open(FILE, encoding='utf-8') as f:
+        total = 0
         for i in f.readlines():
             nb = []
             string = ""
             for j in i:
                 if j.isalpha():
                     string += j
-                    for k in numbers:
+                    for k,v in numbers.items():
                         if k in string:
-                            nb.append(numbers[k])
+                            nb.append(v)
                             string = string[-1] # most important line
                 if j.isdigit():
                     nb.append(j)
 
             nb = str(nb[0]) + str(nb[-1])
-            sum += int(nb)
+            total += int(nb)
 
-    print(sum)
+    print(total)
 
 part2()
-
